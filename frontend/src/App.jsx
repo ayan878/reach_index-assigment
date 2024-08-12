c87918a93c3d33223ad40f4dbf3c7f0d0c5aba1d
ayan878 <ayanraza197@gmail.com>
2024-08-13 04:14:20 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import Sidebar from './components/Sidebar'
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar"; // Adjust the import path as needed
+import Hero from "./components/Hero";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
-    <div><Sidebar/></div>
-  )
+    <div className={darkMode ? "dark" : ""}>
+      <Sidebar darkMode={darkMode} setDarkMode={setDarkMode} />
+      {/* <Hero darkMode={darkMode} setDarkMode={setDarkMode} /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
